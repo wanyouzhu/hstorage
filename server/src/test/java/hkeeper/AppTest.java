cea77fd8c0153a4ba7e0721b41eb4e5b4722cf28
朱万友 <van@highsoft.ltd>
2019-09-29 23:04:59 +0800

    Initial project structur.

---
+++ server/src/test/java/hkeeper/AppTest.java
@@ -0,0 +1,11 @@
+package hkeeper;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+        Assertions.assertThat("TODO").isEqualTo("TODO");
+    }
+}
