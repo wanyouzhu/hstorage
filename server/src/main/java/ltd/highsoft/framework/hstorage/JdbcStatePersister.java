5dfa7bf3e2b2091b3755a62cfc6a30a61d3479c2
朱万友 <van@highsoft.ltd>
2019-10-06 16:08:20 +0800

    Refactored JdbcStatePersister.java

--- server/src/main/java/ltd/highsoft/framework/hstorage/JdbcStatePersister.java
+++ server/src/main/java/ltd/highsoft/framework/hstorage/JdbcStatePersister.java
@@ -17,24 +17,41 @@ public class JdbcStatePersister implements StatePersister {
 
     @Override
     public void saveState(AggregateState state) {
-        String sql = "insert into entities (id, state, timestamp) values (:id, :state, :timestamp) on conflict(id) do update set state = :state, timestamp = :timestamp";
+        jdbcTemplate.update(getSaveCommand(), getSaveArgs(state));
+    }
+
+    private String getSaveCommand() {
+        return ("" +
+            "insert into entities (id, state, timestamp) values (:id, :state, :timestamp) " +
+            "on conflict(id) do update set state = :state, timestamp = :timestamp"
+        );
+    }
+
+    private MapSqlParameterSource getSaveArgs(AggregateState state) {
         MapSqlParameterSource parameterSource = new MapSqlParameterSource();
         parameterSource.addValue("id", state.id());
         parameterSource.addValue("state", state.content(), Types.OTHER);
         parameterSource.addValue("timestamp", Timestamp.from(state.timestamp()));
-        jdbcTemplate.update(sql, parameterSource);
+        return parameterSource;
     }
 
     @Override
     public AggregateState loadState(String id, Class<?> clazz) {
         try {
-            String sql = "select id, state, timestamp from entities where id = :id";
-            return jdbcTemplate.queryForObject(sql, ImmutableMap.of("id", id), new AggregateStateRowMapper());
+            return jdbcTemplate.queryForObject(getLoadCommand(), getLoadArgs(id), new AggregateStateRowMapper());
         } catch (EmptyResultDataAccessException e) {
             throw new AggregateNotFoundException("Aggregate '" + id + "' of type '" + clazz.getName() + "' does not exist!");
         } catch (IncorrectResultSizeDataAccessException e) {
             throw new MalformedDataException("Multiple rows associated to the key '" + id + "'!", e);
         }
+    }
+
+    private String getLoadCommand() {
+        return "select id, state, timestamp from entities where id = :id";
+    }
+
+    private ImmutableMap<String, String> getLoadArgs(String id) {
+        return ImmutableMap.of("id", id);
     }
 
 }
