c4b28eb679f10d0b76a706b575608fdd83fc54f8
朱万友 <van@highsoft.ltd>
2019-10-06 20:36:52 +0800

    DONE: Add class AggregateMarshaller.

---
+++ server/src/test/java/ltd/highsoft/framework/hstorage/AggregateMarshaller.java
@@ -0,0 +1,43 @@
+package ltd.highsoft.framework.hstorage;
+
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+
+import java.io.IOException;
+
+public class AggregateMarshaller {
+
+    private final ObjectMapper mapper;
+
+    public AggregateMarshaller() {
+        this.mapper = createMapper();
+    }
+
+    private ObjectMapper createMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new ParameterNamesModule());
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        return mapper;
+    }
+
+    public String marshal(Object aggregate) {
+        try {
+            return mapper.writeValueAsString(aggregate);
+        } catch (JsonProcessingException e) {
+            throw new MappingException("Mapping error:", e);
+        }
+    }
+
+    public <T> T unmarshal(String value, Class<T> clazz) {
+        try {
+            return mapper.readValue(value, clazz);
+        } catch (JsonMappingException e) {
+            throw new MappingException("Type '" + clazz.getName() + "' is not constructable!", e);
+        } catch (IOException e) {
+            throw new MalformedDataException("Malformed state data!", e);
+        }
+    }
+
+}
---
+++ server/src/test/java/ltd/highsoft/framework/hstorage/AggregateMarshallerTest.java
@@ -0,0 +1,69 @@
+package ltd.highsoft.framework.hstorage;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+class AggregateMarshallerTest {
+
+    private AggregateMarshaller marshaller;
+
+    @BeforeEach
+    void setUp() {
+        marshaller = new AggregateMarshaller();
+    }
+
+    @Test
+    void should_be_able_to_marshal_aggregate_to_string_by_fields() {
+        String expect = "{\"id\":\"one\",\"name\":\"van\"}";
+        assertThat(marshaller.marshal(new TestAggregate("one", "van"))).isEqualTo(expect);
+    }
+
+    @Test
+    void should_reject_object_that_can_not_be_marshalled() {
+        Object object = new NonSerializableObject();
+        Throwable thrown = catchThrowable(() -> marshaller.marshal(object));
+        assertThat(thrown).isInstanceOf(MappingException.class);
+        assertThat(thrown).hasMessage("Mapping error:");
+        assertThat(thrown).hasCauseInstanceOf(JsonProcessingException.class);
+    }
+
+    @Test
+    void should_be_able_to_unmarshal_aggregate_by_fields() {
+        String value = "{\"id\":\"one\",\"name\":\"van\"}";
+        DefaultConstructableAggregate mapped = marshaller.unmarshal(value, DefaultConstructableAggregate.class);
+        assertThat(mapped).hasFieldOrPropertyWithValue("id", "one");
+        assertThat(mapped).hasFieldOrPropertyWithValue("name", "van");
+    }
+
+    @Test
+    void should_be_able_to_unmarshal_aggregate_by_constructor() {
+        String value = "{\"id\":\"one\",\"name\":\"van\"}";
+        FullConstructableAggregate mapped = marshaller.unmarshal(value, FullConstructableAggregate.class);
+        assertThat(mapped).isEqualToComparingFieldByField(new FullConstructableAggregate("one", "van"));
+    }
+
+    @Test
+    void should_reject_malformed_state_during_unmarshalling() {
+        String malformedValue = "{\"id\":\"one\",\"name\":}";
+        Throwable thrown = catchThrowable(() -> marshaller.unmarshal(malformedValue, TestAggregate.class));
+        assertThat(thrown).isInstanceOf(MalformedDataException.class);
+        assertThat(thrown).hasMessage("Malformed state data!");
+        assertThat(thrown).hasCauseInstanceOf(IOException.class);
+    }
+
+    @Test
+    void should_reject_unmarshallable_types_during_unmarshalling() {
+        String value = "{\"id\":\"one\",\"name\":\"van\"}";
+        Throwable thrown = catchThrowable(() -> marshaller.unmarshal(value, AbstractAggregate.class));
+        assertThat(thrown).isInstanceOf(MappingException.class);
+        assertThat(thrown).hasMessage("Type 'ltd.highsoft.framework.hstorage.AbstractAggregate' is not constructable!");
+        assertThat(thrown).hasCauseInstanceOf(JsonMappingException.class);
+    }
+
+}
