9432b72549d559ad27fc8ece2c18b63bd9fb01d4
朱万友 <van@highsoft.ltd>
2019-10-06 12:50:50 +0800

    Refactored JdbcStatePersisterTest.java

--- server/src/test/java/ltd/highsoft/framework/hstorage/JdbcStatePersisterTest.java
+++ server/src/test/java/ltd/highsoft/framework/hstorage/JdbcStatePersisterTest.java
@@ -11,17 +11,18 @@ import static org.assertj.core.api.Assertions.*;
 class JdbcStatePersisterTest {
 
     private TestDatabase testDatabase;
+    private StatePersister persister;
 
     @BeforeEach
     void setUp() {
         testDatabase = new TestDatabase();
         testDatabase.recreateCollectionTable();
+        persister = new JdbcStatePersister(testDatabase.jdbcTemplate());
     }
 
     @Test
     void should_be_able_to_save_state_to_database() {
         AggregateState state = new AggregateState("one", "{\"id\": \"one\"}", Instant.EPOCH);
-        StatePersister persister = new JdbcStatePersister(testDatabase.jdbcTemplate());
         persister.saveState(state);
         Map<String, Object> loaded = testDatabase.getSavedAggregateState();
         assertThat(loaded.get("id")).isEqualTo("one");
@@ -32,11 +33,9 @@ class JdbcStatePersisterTest {
     @Test
     void should_be_able_to_load_state_from_database() {
         AggregateState state = new AggregateState("one", "{\"id\": \"one\"}", Instant.EPOCH);
-        StatePersister persister = new JdbcStatePersister(testDatabase.jdbcTemplate());
         persister.saveState(state);
         AggregateState loaded = persister.loadState("one", TestAggregate.class);
         assertThat(loaded).isEqualToComparingFieldByField(state);
     }
-
 
 }
